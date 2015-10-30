c3dfb8bddc345e0b5a34c44c00adcdef3c25db5b
Scott Evans <git@scott.ee>
2015-10-30 10:26:05 +0000

    Basic structure

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== WordCamp Europe CSS 2016 ===
+Contributors: scottsweb
+Tags: wordcamp, wceu, css, 2016, vienna, austria, gulp
+Requires at least: 3.0
+Tested up to: 4.4
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Custom CSS for the WordCamp Europe 2016 theme.
+
+== Description ==
+
+
+
+CSS generated from this plugin needs to be copy and pasted into the `Appearance -> Edit CSS` screen.
+
+
+[a plugin by Scott Evans](http://scott.ee/ "WordPress designer and developer")
+
+== Installation ==
+
+To install this plugin:
+
+1. Upload the `wceu-2016-css` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. That's it!
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+1. Instagram widget on the front end
+2. Instagram widget in the theme customiser
+
---
+++ wceu-2016-css.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: WCEU 2016 CSS
+Plugin URI: https://github.com/scottsweb/wceu-2016-css
+Description: Custom CSS for the WordCamp Europe 2016 theme.
+Version: 1.0
+Author: Scott Evans
+Author URI: http://scott.ee
+Text Domain: wceu-2016-css
+Domain Path: /languages/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+define( 'WCEU_2016_CSS_URL', plugins_url( '/css', __FILE__ ) );
+define( 'WCEU_2016_PATH', dirname( __FILE__ ) );
+define( 'WCEU_2016_BASE', plugin_basename( __FILE__ ) );
+define( 'WCEU_2016_FILE', __FILE__ );
+
+/**
+ * Enqueue the compiled stylesheet
+ *
+ * @return void
+ */
+function wceu_css_setup() {
+	wp_enqueue_style( 'wceu', WCEU_2016_CSS_URL . '/styles.css', array(), '1.0', 'screen' );
+}
+add_action( 'wp_enqueue_scripts', 'wceu_css_setup' );
